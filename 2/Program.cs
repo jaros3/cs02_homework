257dc43ea08b690f574739bed369dcdd3fe88b23
SaiqG <akkorsakov@gmail.com>
2022-09-02 23:21:05 +0300

    HW commit

---
+++ 2/Program.cs
@@ -0,0 +1,56 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+while (true) // Зацикливание программы
+{
+    int NumChecker() //метод проверки на ввод числа
+    {
+        int isItNum = -1;
+        bool check;
+        string num = "q";
+        Console.WriteLine("   //число для подолжения, или *q* чтоб выйти");
+        do
+        {
+
+            num = Console.ReadLine();
+            if (num == "q")
+            {
+
+                break;
+            }
+            else
+            {
+                check = int.TryParse(num, out isItNum);
+            }
+        } while (check == false);
+
+        if (num == "q")
+        {
+            Console.WriteLine($"Вы не ввели число");
+            System.Environment.Exit(0); //завершение программы на любом этапе
+        }
+        return isItNum;
+    }
+
+    //Основной код дз
+    //------------------------------------------------------------------------------------------
+    Console.Write("Введите число.");
+
+    int Num = NumChecker(); //вместо обычного ReadLine, сразу проверяет, что число целое
+    string numSt = Num.ToString(); //преобразуем обратно в строку, чтобы можно было достать 3е число без лишних действий
+
+    if (Num / 100 > 0) //проверяем, что число содержит 3ю цифру
+    {
+        Console.WriteLine($"Третья цифра в {Num} -> {numSt[2]}");
+    }
+    else
+    {
+        Console.WriteLine($"В {Num} нет третей цифры");
+    }
+    //------------------------------------------------------------------------------------------
+
+    //Зацикливание программы
+
+    Console.WriteLine("Enter чтобы продолжить / q чтобы выйти");
+    ConsoleKeyInfo quite = Console.ReadKey();
+    Console.WriteLine();
+    if (quite.KeyChar == 'q') break;
+}
---
+++ 3/Program.cs
@@ -0,0 +1,26 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+while (true) // Зацикливание программы
+{
+    Console.WriteLine("Введите день недели по счету.");
+    ConsoleKeyInfo day = Console.ReadKey(); // в задаче говорится про цифру, а не число, решил сделать через ReadKey
+    Console.WriteLine();
+
+    int wDay = day.KeyChar - 48; // номер цифры 1 это 49, 2 - 50 и тд, по этому -48
+
+    if (wDay > 5 & wDay < 8)
+    {
+        Console.WriteLine("Ура Выходной!");
+    }
+    else if (wDay < 6)
+    {
+        Console.WriteLine("Нее, сегодня опять на работку((");
+    }
+    else Console.WriteLine("Друг, в неделе всего 7 дней, порпобуй еще.");
+
+    //Зацикливание программы   
+
+    Console.WriteLine("Enter чтобы продолжить / q чтобы выйти");
+    ConsoleKeyInfo quite = Console.ReadKey();
+    Console.WriteLine();
+    if (quite.KeyChar == 'q') break;
+}
