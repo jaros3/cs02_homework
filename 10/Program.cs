1745cade493b5b960a9b0194fdf77eeb70d2c0a9
Arthur Patrakhaev <redred05@mail.ru>
2022-09-02 23:22:37 +0300

    initial commit

---
+++ 10/Program.cs
@@ -0,0 +1,5 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введи трехзначное число");
+string threeDigitNumber = Console.ReadLine();
+Console.WriteLine($"Вторая цифра числа {threeDigitNumber[1]}");
