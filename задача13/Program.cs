8f8e907399608ace965225b532b74ddc21588efe
29Tatiana <amiravob@mail.ru>
2022-09-03 23:39:59 +0300

    задача13ред1

--- задача13/Program.cs
+++ задача13/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Введите трехзначное число");
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите трехзначное число");
 int number = int.Parse(Console.ReadLine());
 if (number > 100)
     Console.WriteLine($"{number % 10}");
