863a88b8e53e2e1d879a5385d50bb3a43a2b855d
Vladimir <hahapupkin@gmail.com>
2022-09-03 15:42:53 +0400

    complete homework

---
+++ task10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("enter a number consisting of three elements");
+
+int number = int.Parse(Console.ReadLine());
+
+if (number >= 100 && number <= 999)
+    {
+        Console.WriteLine("second element of number is: " + (number / 10) % 10);
+
+    }
+else
+    {
+        Console.WriteLine("---wrong number---");
+    }
---
+++ task13/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("enter a number consisting of three elements");
+
+int number = int.Parse(Console.ReadLine());
+
+if (number >= 100 && number <= 999)
+    {
+        Console.WriteLine("third element of number is: " + number % 10);
+
+    }
+else if(number <= 99 || number >= 1000)
+    {
+        Console.WriteLine("--third digit does not exist or these are more then three digits--");
+    }
