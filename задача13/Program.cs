41b8f0804b58c025025f0ac77d1763a88122c2df
29Tatiana <amiravob@mail.ru>
2022-09-03 23:13:20 +0300

    задача13ред

---
+++ задача13/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int number = int.Parse(Console.ReadLine());
+if (number > 100)
+    Console.WriteLine($"{number % 10}");
+else
+    Console.WriteLine("третьей цифры нет");
+
+
