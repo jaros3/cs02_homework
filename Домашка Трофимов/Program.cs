2011331364381d62adccc71b0e40176be11e6357
arseny <arseniy.trofimov1993@gmail.com>
2022-09-03 18:58:26 +0300

    Problem 10

---
+++ Домашка Трофимов/Program.cs
@@ -0,0 +1,20 @@
+﻿void FindSecondDigit(int number)
+{
+
+    if (number > 99)
+    {   
+        int SecondDigit = (number / 10) % 10;
+
+        Console.WriteLine($"Вторая цифра трехзначного числа: {SecondDigit}");
+    }
+    else 
+    {
+        Console.WriteLine("Введите трехзначное число!");
+    }
+}   
+
+Console.WriteLine("Введите трехзначное число: ");
+
+int d = int.Parse(Console.ReadLine());
+
+FindSecondDigit(d);
