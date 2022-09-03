2ef25c27e0b0683f8ce22d6be4eb3da0a54b8c03
SaiqG <akkorsakov@gmail.com>
2022-09-03 13:57:31 +0300

    Fix hw

--- 3/Program.cs
+++ 3/Program.cs
@@ -1,26 +1,27 @@
 ﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 while (true) // Зацикливание программы
 {
-    Console.WriteLine("Введите день недели по счету.");
+    Console.WriteLine();
+    Console.WriteLine("Введите день недели по счету или *q* для выхода");
     ConsoleKeyInfo day = Console.ReadKey(); // в задаче говорится про цифру, а не число, решил сделать через ReadKey
     Console.WriteLine();
 
     int wDay = day.KeyChar - 48; // номер цифры 1 это 49, 2 - 50 и тд, по этому -48
-
-    if (wDay > 5 & wDay < 8)
+    string[] dayName = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+    if (wDay > 5 && wDay < 8)
     {
-        Console.WriteLine("Ура Выходной!");
+        Console.WriteLine($"{dayName[wDay - 1]} - Ура Выходной!");
     }
     else if (wDay < 6)
     {
-        Console.WriteLine("Нее, сегодня опять на работку((");
+        Console.WriteLine($"{dayName[wDay - 1]} - На работку((");
     }
-    else Console.WriteLine("Друг, в неделе всего 7 дней, порпобуй еще.");
+    else if (day.KeyChar == 'q') // Выход из while
+    {
+        Console.WriteLine("Приходите еще!");
+        break;
+    }
 
-    //Зацикливание программы   
+    else { Console.WriteLine("Друг, в неделе всего 7 дней, порпобуй еще."); }
 
-    Console.WriteLine("Enter чтобы продолжить / q чтобы выйти");
-    ConsoleKeyInfo quite = Console.ReadKey();
-    Console.WriteLine();
-    if (quite.KeyChar == 'q') break;
 }
