53c184d3fa1e6687b1b775739973ef699e739a2d
Pavel <p.a.dudarev@mail.ru>
2022-09-06 17:10:16 +0500

    Second task in another solution

---
+++ Task 021/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int count = 0;
+int digit = 0;
+int currentNumber = number;
+
+
+while(currentNumber!=0)
+{
+    currentNumber = currentNumber / 10;
+    count++;
+
+}
+
+if(count == 3)
+{
+    digit = number % 10;
+    Console.WriteLine($"Третья цифра = {digi
+
+}
+if (count > 3)
+{
+for(int i = 0;i < count - 3; i++)
+{
+    number = number / 10;
+}
+   
+    
+ digit = number % 10;
+ Console.WriteLine($"Третья цифра = {digit}"
+    
+}
+if (count < 3)
+{
+    Console.WriteLine("Третьей цифры нет");
+
+}
