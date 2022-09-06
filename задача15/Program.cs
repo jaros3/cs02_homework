0c5dc91bba931cb8d8abe6bd1148fd4b616ace84
29Tatiana <amiravob@mail.ru>
2022-09-06 17:48:59 +0300

    задача15ред2

--- задача15/Program.cs
+++ задача15/Program.cs
@@ -3,14 +3,12 @@ Console.WriteLine("Введите день недели от 1 до 7");
 int number = int.Parse(Console.ReadLine());
 if (number < 1 || number > 7)
     Console.WriteLine("Это не день недели, введите число от 1 до 7");
+else if (number == 6 || number == 7)
+{
+    Console.WriteLine("да");
+}
 else
 {
-    if (number == 6 || number == 7)
-    {
-        Console.WriteLine("да");
-    }
-    else
-    {
-        Console.WriteLine("нет");
-    }
+    Console.WriteLine("нет");
 }
+
