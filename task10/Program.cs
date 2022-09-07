43581c8727262aa132e9e3d9f230bd83aee7b34f
Karina Babakova <taytynovak@mail.ru>
2022-09-07 15:17:43 +0700

    Вторая цифра от трехзначного числа

---
+++ task10/Program.cs
@@ -0,0 +1,8 @@
+﻿//На вход трехзначное число, на выход вторую цифру этого числа
+
+Console.WriteLine("Введите трехзначное число ");
+int number = int.Parse(Console.ReadLine());
+
+int result = (number/10) % 10;
+
+Console.WriteLine(result); 
