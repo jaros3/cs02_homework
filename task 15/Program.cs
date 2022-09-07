1f58ad38edf61bdb2a0ac5c2d8313bd1b57f79d1
Karina Babakova <taytynovak@mail.ru>
2022-09-07 17:51:05 +0700

    Дни недели

---
+++ task 15/Program.cs
@@ -0,0 +1,15 @@
+﻿// На вход цифра обозначающая день недели, и проверяет, является ли этот день выходным
+
+Console.WriteLine("Введите цифру ");
+int digit = int.Parse(Console.ReadLine());
+
+if(digit <= 5 && digit > 0) {
+    Console.WriteLine($"День {digit} будний" );
+}
+else if(digit <= 7 && digit > 0) {
+    Console.WriteLine($"День {digit} выходной");
+}
+else {
+    Console.WriteLine($"День {digit} введен неверно");
+}
+
