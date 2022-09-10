e98d2f89318df6b4d10e0b2fc587cd5c8d2c69b1
Lilia Bezrukova <lilia_dyatlova@mail.ru>
2022-09-10 21:59:57 +0400

    Zadacha 015

---
+++ dz015/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите порядковый номер дня недели:");
+int number = int.Parse(Console.ReadLine());
+
+if (number < 1 || number > 7)
+    Console.WriteLine("Введены некорректные данные");
+else if (number == 6 || number == 7)
+    Console.WriteLine("Это выходной!");
+else
+    Console.WriteLine("Это рабочий день");
