2245708580cfafda86d2f65e7590333a7d9df171
Karina Babakova <taytynovak@mail.ru>
2022-09-10 19:27:21 +0700

    Задача 23: Таблица кубов чисел от 1 до N

---
+++ task3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа которая принимает на вход число N и выдает таблицу кубов чисел от 1 до N.
+Console.WriteLine("Введите число ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+
+    Console.Write($"{Math.Pow(i, 3)}");
+    if (i < n)
+    {
+        Console.Write(",");
+    }
+}
+Console.WriteLine();
