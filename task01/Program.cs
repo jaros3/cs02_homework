7aaf4a8dc0053e80976cd4280e86723402f54182
PipBoy <pk7@ya.ru>
2022-09-12 02:34:11 +0700

    CorrectedWithTeacherComment

--- task01/Program.cs
+++ task01/Program.cs
@@ -5,6 +5,8 @@ int numberA = new Random().Next(100, 1000);
 
 Console.WriteLine($"{numberA} - Сгенерированное случайное трехзначное число");
 
-int digit2 = ((numberA /10) % 10);
+// int digit2 = ((numberA /10) % 10);  В строке 8 не нужны ни внешние, ни даже внутренние скобки. У операции % нет приоритета перед
+
+int digit2 = numberA / 10 % 10;
 
 Console.WriteLine ($"{digit2} - Вторая цифра этого числа");
