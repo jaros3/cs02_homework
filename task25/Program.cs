5983f35172d504c92fce4d3ef63bb416a06e7b1d
Karina Babakova <taytynovak@mail.ru>
2022-09-15 11:38:43 +0700

    Основа для 25 задачи

---
+++ task25/Program.cs
@@ -0,0 +1,9 @@
+﻿// 
+Console.WriteLine("Введите число А ");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B ");
+int numberB = int.Parse(Console.ReadLine());
+
+double degree = Math.Pow(numberA, numberB);
+
+Console.WriteLine($"{degree}");
