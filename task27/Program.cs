ae5141adad89b9183f9074e6311941213574057b
Karina Babakova <taytynovak@mail.ru>
2022-09-15 12:06:04 +0700

    Задача27

---
+++ task27/Program.cs
@@ -0,0 +1,20 @@
+﻿// 
+int FindSumOfDigits(int num)
+{ 
+    int sum = 0;
+    while (num > 0)
+    {
+        sum += num%10;
+        num = num/10;
+    }
+    return sum;
+}
+Console.Write("Введите N. ");
+bool isNumCorrect = false;
+int inputNum = 0;
+while (!isNumCorrect)
+{
+    Console.WriteLine("Число должно быть целым. ");
+    isNumCorrect = int.TryParse(Console.ReadLine()!, out inputNum);
+}
+Console.WriteLine($"Сумма цифр в числе {inputNum} это {FindSumOfDigits(inputNum)}");
